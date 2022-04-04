35b32df4b534d8bba747fb45e8093e7de72d0478
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-04 21:11:12 +0000

    Added 2 Versions, one print, one assigning different variables and switching them.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+# Version 01
+# assign the input for a and b
+# a = input("a:")
+# b = input("b:")
+# # print the switched variables
+# print(input("a:" + b) + ("b:" + a))
+
+# Version 02
+# assign the input for a and b
+# Dont change:
 a = input("a:")
 b = input("b:")
+# Write code here:
+c = a
+a = b
+d = b
+b = c
 
-print(input("a:" + b) + ("b:" + a))
-
+# Dont change:
+print("a= " + a)
+print("b= " + b)
